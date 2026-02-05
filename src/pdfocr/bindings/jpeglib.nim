ee47ce52e87881209493794ef2691eb8732ee9be
Antonis Geralis <b3lievem@yandex.com>
2026-02-05 13:38:57 +0200

    Debug linking issues

--- src/pdfocr/bindings/jpeglib.nim
+++ src/pdfocr/bindings/jpeglib.nim
@@ -1,11 +1,12 @@
-# Dynamic library configuration
-
+# We need stdio.h before jpeglib.h for FILE type
 {.passC: "-include stdio.h".}
-when defined(windows):
+
+when hostOS == "windows":
   const jpegDll = "libjpeg-62.dll"
-  {.push dynlib: jpegDll, header: "jpeglib.h".}
+  {.push dynlib: jpegDll.}
 else:
-  {.push header: "jpeglib.h".}
+  {.passC: "-include stdio.h".}
+  {.push header: "<jpeglib.h>".}
 
 # Standard JPEGLib types
 type
@@ -14,7 +15,7 @@ type
   
   # We only define fields we read/write.
   # The "incompleteStruct" tells Nim to ignore size mismatches.
-  jpeg_compress_struct* {.importc: "struct jpeg_compress_struct", incompleteStruct, pure.} = object
+  jpeg_compress_struct* {.importc: "struct jpeg_compress_struct", header: "<jpeglib.h>", incompleteStruct, pure.} = object
     err*: ptr jpeg_error_mgr
     image_width*: cuint
     image_height*: cuint
@@ -23,12 +24,12 @@ type
     next_scanline*: cuint
     # We omit the hundreds of private fields; C handles them!
   
-  jpeg_common_struct* {.importc: "struct jpeg_common_struct", incompleteStruct, pure.} = object
+  jpeg_common_struct* {.importc: "struct jpeg_common_struct", header: "<jpeglib.h>", incompleteStruct, pure.} = object
     err*: ptr jpeg_error_mgr # Pointer to the error manager
   
   # We only list the function pointers we need to override or call.
   # The C compiler handles the rest of the fields/size automatically.
-  jpeg_error_mgr* {.importc: "struct jpeg_error_mgr", incompleteStruct, pure.} = object
+  jpeg_error_mgr* {.importc: "struct jpeg_error_mgr", header: "<jpeglib.h>", incompleteStruct, pure.} = object
     error_exit*: proc (cinfo: j_common_ptr) {.cdecl.}
     emit_message*: proc (cinfo: j_common_ptr, msg_level: cint) {.cdecl.}
     output_message*: proc (cinfo: j_common_ptr) {.cdecl.}
@@ -49,15 +50,18 @@ const
   JMSG_LENGTH_MAX* = 200
 
 # --- Function Imports ---
-proc jpeg_std_error*(err: ptr jpeg_error_mgr): ptr jpeg_error_mgr {.importc, cdecl.}
-proc jpeg_create_compress*(cinfo: ptr jpeg_compress_struct) {.importc, cdecl.}
-proc jpeg_stdio_dest*(cinfo: ptr jpeg_compress_struct, outfile: File) {.importc, cdecl.}
-proc jpeg_mem_dest*(cinfo: ptr jpeg_compress_struct, outbuffer: ptr ptr byte, outsize: ptr culong) {.importc, cdecl.}
-proc jpeg_set_defaults*(cinfo: ptr jpeg_compress_struct) {.importc, cdecl.}
-proc jpeg_set_quality*(cinfo: ptr jpeg_compress_struct, quality: cint, force_baseline: cint) {.importc, cdecl.}
-proc jpeg_start_compress*(cinfo: ptr jpeg_compress_struct, write_all_tables: cint) {.importc, cdecl.}
-proc jpeg_write_scanlines*(cinfo: ptr jpeg_compress_struct, scanlines: JSAMPARRAY, num_lines: cuint): cuint {.importc, cdecl.}
-proc jpeg_finish_compress*(cinfo: ptr jpeg_compress_struct) {.importc, cdecl.}
-proc jpeg_destroy_compress*(cinfo: ptr jpeg_compress_struct) {.importc, cdecl.}
+{.push cdecl, importc.}
 
+proc jpeg_std_error*(err: ptr jpeg_error_mgr): ptr jpeg_error_mgr
+proc jpeg_create_compress*(cinfo: ptr jpeg_compress_struct)
+proc jpeg_stdio_dest*(cinfo: ptr jpeg_compress_struct, outfile: File)
+proc jpeg_mem_dest*(cinfo: ptr jpeg_compress_struct, outbuffer: ptr ptr byte, outsize: ptr culong)
+proc jpeg_set_defaults*(cinfo: ptr jpeg_compress_struct)
+proc jpeg_set_quality*(cinfo: ptr jpeg_compress_struct, quality: cint, force_baseline: cint)
+proc jpeg_start_compress*(cinfo: ptr jpeg_compress_struct, write_all_tables: cint)
+proc jpeg_write_scanlines*(cinfo: ptr jpeg_compress_struct, scanlines: JSAMPARRAY, num_lines: cuint): cuint
+proc jpeg_finish_compress*(cinfo: ptr jpeg_compress_struct)
+proc jpeg_destroy_compress*(cinfo: ptr jpeg_compress_struct)
+
+{.pop.}
 {.pop.}
--- tests/test_curl_bindings.nims
+++ tests/test_curl_bindings.nims
@@ -12,7 +12,6 @@ when defined(macosx):
   switch("passL", "-L" & staticExec("brew --prefix curl") & "/lib")
 elif defined(windows):
   # Windows: MinGW-Builds + curl from chocolatey
-  switch("gcc.path", "C:/mingw64/bin")
   let curlRoot = getEnv("CURL_ROOT")
   switch("passC", "-I" & curlRoot & "/include")
   switch("passL", "-L" & curlRoot & "/lib")
