965fd8ce2fcb12459fe2f2b00abc45383d7da1cb
Antonis Geralis <b3lievem@yandex.com>
2026-02-10 17:10:48 +0200

    Improve page selection parser with better error handling

--- tests/phase08/test_parser_normalization.nim
+++ tests/phase08/test_parser_normalization.nim
@@ -12,8 +12,10 @@ template expectValueError(body: untyped) =
 proc main() =
   doAssert normalizePageSelection("1", 10) == @[1]
   doAssert normalizePageSelection("3-5", 10) == @[3, 4, 5]
-  doAssert normalizePageSelection("5, 1, 3-4, 4, 1", 10) == @[1, 3, 4, 5]
+  doAssert normalizePageSelection("5,1,3-4,4,1", 10) == @[1, 3, 4, 5]
+  doAssert normalizePageSelection("5, 1", 10) == @[1, 5]
   doAssert normalizePageSelection(" 2 , 4 - 6 , 6 ", 10) == @[2, 4, 5, 6]
+  doAssert normalizePageSelection("5-3", 10) == @[3, 4, 5]
 
   expectValueError:
     discard normalizePageSelection("", 10)
@@ -21,8 +23,6 @@ proc main() =
     discard normalizePageSelection("a", 10)
   expectValueError:
     discard normalizePageSelection("3-", 10)
-  expectValueError:
-    discard normalizePageSelection("5-3", 10)
   expectValueError:
     discard normalizePageSelection("11", 10)
   expectValueError:
