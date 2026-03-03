090998032fd4af774aae08421dee4cb6186b99de
Antonis Geralis <b3lievem@yandex.com>
2026-03-03 15:56:32 +0200

    Remove code that overallocates

--- src/pdfocr/page_selection.nim
+++ src/pdfocr/page_selection.nim
@@ -1,9 +1,9 @@
 import std/[algorithm, parseutils]
 
 proc allPagesSelection*(totalPages: int): seq[int] =
-  result = newSeqOfCap[int](totalPages)
-  for page in 1..totalPages:
-    result.add(page)
+  result = newSeq[int](totalPages)
+  for idx in 0..<totalPages:
+    result[idx] = idx + 1
 
 proc parsePageAt(spec: string; idx: var int): int =
   let consumed = parseInt(spec, result, idx)
